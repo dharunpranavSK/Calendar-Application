52050d757800ff75c14729b5ecebf3b67de5869f
dharunDP <dharunpranav.ad22@bitsathy.ac.in>
2025-06-25 20:58:18 +0530

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/calendar-app/', // <-- your repo name
+  base: '/Calendar-Application/', // <-- your repo name
 })
