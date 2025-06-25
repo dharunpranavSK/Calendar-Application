3db34bc4b9a39ef5f9b80432fc878661fc1ce2d1
dharunDP <dharunpranav.ad22@bitsathy.ac.in>
2025-06-25 21:04:50 +0530

    Final commit

--- src/components/Topbar/TopBar.jsx
+++ src/components/Topbar/TopBar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => {
     <div className="topbar">
       <h1>My Calendar App</h1>
       <img
-        src="/ChatGPT Image Jun 25, 2025, 08_35_32 PM.png"  // Place your image in public/profile.jpg or update the path
+        src="public/dpWhiteLogo.png"  // Place your image in public/profile.jpg or update the path
         alt="Profile"
         className="topbar-profile"
       />
